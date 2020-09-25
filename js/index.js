895a51b3668184513664f799e861dcd2703f83ae
bengbengku <anggiatbenget70@gmail.com>
2020-09-25 20:09:56 +0700

    Struktur Html Pembuatan Awal

---
+++ js/index.js
@@ -0,0 +1,47 @@
+const URL = "http://localhost:3000/tweets";
+
+/**
+ * Mengambil Data Twitter dari API
+ */
+const getTwitterData = () => {
+
+}
+
+/**
+ * Menyimpan data page selanjutnya
+ */
+const saveNextPage = (metadata) => {
+}
+
+/**
+ * Menghandle saat pengguna mengklik trend
+ */
+const selectTrend = (e) => {
+}
+
+/**
+ * Atur visibilitas halaman berikutnya berdasarkan jika ada data di halaman berikutnya
+ */
+const nextPageButtonVisibility = (metadata) => {
+}
+
+/**
+ * Buat HTML Tweet berdasarkan Data dari API
+ */
+const buildTweets = (tweets, nextPage) => {
+
+}
+
+/**
+ * Bangun HTML untuk Gambar Tweet
+ */
+const buildImages = (mediaList) => {
+
+}
+
+/**
+ * Bangun HTML untuk video Tweet
+ */
+const buildVideo = (mediaList) => {
+
+}

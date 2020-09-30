f1bc29a8867d723c484491b6ea97f2d12f50b40f
bengbengku <anggiatbenget70@gmail.com>
2020-09-30 20:26:47 +0700

    setting-search-component

--- js/index.js
+++ js/index.js
@@ -1,60 +1,59 @@
 const URL = "http://localhost:3000/tweets";
 
 
+//Trik membuat tombol enter dalam pencarian melaui code key = "Enter"
+const onEnter = (e) => {
+    if(e.key == "Enter"){
+        getTwitterData();
+    }
+}
 
 /**
  * Mengambil Data Twitter dari API
  */
 const getTwitterData = () => {
+  const query = document.getElementById("user-search-input").value;
 
-    const query = document.getElementById('user-search-input').value;
-    const url = "http://localhost:3000/tweets?q=coding&count=10";
-    console.log(query);
+  if (!query) return;
+  const encodedQuery = encodeURIComponent(query);
+  const fullUrl = `${URL}?q=${encodedQuery}&count=10`; //pencarian
 
-    fetch(url).then((response) => {
-        return response.json();
-    }).then((data) => {
-        console.log(data);
+  fetch(fullUrl)
+    .then((response) => {
+      return response.json();
     })
-
-}
+    .then((data) => {
+      console.log(data);
+    });
+};
 getTwitterData();
 
 /**
  * Menyimpan data page selanjutnya
  */
-const saveNextPage = (metadata) => {
-}
+const saveNextPage = (metadata) => {};
 
 /**
  * Menghandle saat pengguna mengklik trend
  */
-const selectTrend = (e) => {
-}
+const selectTrend = (e) => {};
 
 /**
  * Atur visibilitas halaman berikutnya berdasarkan jika ada data di halaman berikutnya
  */
-const nextPageButtonVisibility = (metadata) => {
-}
+const nextPageButtonVisibility = (metadata) => {};
 
 /**
  * Buat HTML Tweet berdasarkan Data dari API
  */
-const buildTweets = (tweets, nextPage) => {
-
-}
+const buildTweets = (tweets, nextPage) => {};
 
 /**
  * Bangun HTML untuk Gambar Tweet
  */
-const buildImages = (mediaList) => {
-
-}
+const buildImages = (mediaList) => {};
 
 /**
  * Bangun HTML untuk video Tweet
  */
-const buildVideo = (mediaList) => {
-
-}
+const buildVideo = (mediaList) => {};
